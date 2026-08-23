23a79a97bc7ff7db0312dfc1b37f73d0b614c867
bnbgrs <bnbgrs@googlemail.com>
2026-08-23 14:58:04 +0200

    fix: allow adjacent research tab-order pairs

--- src/athena/desktop/pathena_research_experience_2500.py
+++ src/athena/desktop/pathena_research_experience_2500.py
@@ -344,7 +344,7 @@ class PathenaResearchExperience(QObject):
             self.workspace.refresh_button,
             self.workspace.cancel_button,
         )
-        for first, second in zip(focus_order, focus_order[1:], strict=True):
+        for first, second in zip(focus_order, focus_order[1:]):
             QWidget.setTabOrder(first, second)
 
         self._add_shortcut("Ctrl+Enter", self._start_if_available)
