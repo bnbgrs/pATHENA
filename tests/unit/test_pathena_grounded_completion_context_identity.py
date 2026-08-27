5dd645d7281e63af89c7f8c337f310556084c7ce
bnbgrs <bnbgrs@googlemail.com>
2026-08-27 21:30:22 +0200

    test: pin grounded run in completion context fixtures

--- tests/unit/test_pathena_grounded_completion_context_identity.py
+++ tests/unit/test_pathena_grounded_completion_context_identity.py
@@ -10,6 +10,7 @@ from athena.chat.grounded_completion import (
     GroundedSendCompletionCorruptionError,
     GroundedSendCompletionRepository,
 )
+from athena.chat.grounded_processing_run import bind_grounded_processing_run
 from athena.chat.grounded_send import GroundedSendCoordinator
 from athena.chat.repository import ChatRepository
 from athena.chat.request_fingerprint import ChatSendMode, build_chat_request_fingerprint
@@ -159,6 +160,14 @@ def _assistant_committed(database: SQLiteDatabase):
         model_signature_id=package.model_signature.model_signature_id,
         prompt_template_id="completion-context-identity-test",
         prompt_template_version="1",
+    )
+    bind_grounded_processing_run(
+        database,
+        operation_id=operation_id,
+        chat_id=chat_id,
+        processing_run_id=run.processing_run_id,
+        package=package,
+        trigger_actor_id=user,
     )
     coordinator.begin_provider_attempt(
         operation_id=operation_id,
