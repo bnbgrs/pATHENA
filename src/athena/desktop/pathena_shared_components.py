fb3fc68b7458618715e10d3eec3873ad1053803b
bnbgrs <bnbgrs@googlemail.com>
2026-09-06 06:50:06 +0200

    ui: integrate Library detail-reader focus state

--- src/athena/desktop/pathena_shared_components.py
+++ src/athena/desktop/pathena_shared_components.py
@@ -283,6 +283,9 @@ QComboBox:focus,
 QSpinBox:focus,
 QDoubleSpinBox:focus,
 QListWidget:focus,
+QPlainTextEdit#persistentKnowledgeDetails:focus,
+QPlainTextEdit#persistentClaimDetails:focus,
+QPlainTextEdit#semanticReviewDetails:focus,
 QWidget[pathenaKeyboardFocus="true"] {{
     border: 1px solid {PALETTE.accent};
 }}
