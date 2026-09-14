6fce1d686f43d4cb8e3ea060055f4a871147df82
bnbgrs <bnbgrs@googlemail.com>
2026-09-14 04:44:54 +0200

    ui: widen chat inspector to reference proportion

--- src/athena/desktop/pathena_design_tokens.py
+++ src/athena/desktop/pathena_design_tokens.py
@@ -86,7 +86,7 @@ class ShellGeometry:
     top_bar_height: int = 60
     icon_rail_width: int = 248
     secondary_nav_width: int = 220
-    inspector_width: int = 360
+    inspector_width: int = 390
     composer_min_height: int = 58
     composer_action_size: int = 44
 
