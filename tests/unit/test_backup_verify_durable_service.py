073cb77eebd7cdc3b5bd43e7d4463cf147f24c1e
bnbgrs <bnbgrs@googlemail.com>
2026-09-17 01:29:05 +0200

    Backend: fix Ruff first-party import grouping

--- tests/unit/test_backup_verify_durable_service.py
+++ tests/unit/test_backup_verify_durable_service.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from unittest.mock import Mock
 
+import pytest
+
 from athena.jobs.backup_verify_durable_service import BackupDeepVerifyDurableJobService
 from athena.jobs.backup_verify_payload import (
     BACKUP_VERIFY_DEEP_JOB_TYPE,
@@ -9,7 +11,6 @@ from athena.jobs.backup_verify_payload import (
 )
 from athena.jobs.models import JobPriority
 from athena.jobs.service import InvalidJobPayloadError
-import pytest
 
 
 _SNAPSHOT_ID = "12345678-1234-5678-9234-567812345678"
