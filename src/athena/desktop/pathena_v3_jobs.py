2d19f165c5b25e71c801d66a68b2e04a9726d718
bnbgrs <bnbgrs@googlemail.com>
2026-09-25 02:10:22 +0200

    polish(ui): keep Jobs command status on one readable line

--- src/athena/desktop/pathena_v3_jobs.py
+++ src/athena/desktop/pathena_v3_jobs.py
@@ -54,6 +54,8 @@ class PathenaV3JobsController(QObject):
 
         workspace.status.setParent(command)
         workspace.status.setObjectName("v3JobsStatus")
+        workspace.status.setWordWrap(False)
+        workspace.status.setMinimumWidth(180)
         workspace.status.show()
         layout.addWidget(workspace.status)
 
